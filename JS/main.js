e52bc3e87cccde52c7557054d4ad40d1587a1399
Kisalaya Kishan <kislay.kishan007@gmail.com>
2022-03-15 02:21:18 +0530

    update main.js

--- JS/main.js
+++ JS/main.js
@@ -49,8 +49,8 @@ function getMeaning(wordMeaning)
 const keys = [
 
     "Q","W","E","R","T","Y","U","I","O","P",
-    "A","S","D","F","G","H","J","K","L","Enter",
-    "Z","X","C","V","B","N","M","<<",
+    "A","S","D","F","G","H","J","K","L","<<",
+    "Z","X","C","V","B","N","M","Enter",
  ]
 
 
